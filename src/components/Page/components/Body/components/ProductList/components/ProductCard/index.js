003388ca8508280877ac0108e6a360a4a12d3452
procopio420 <lucas-procopio@outlook.com>
2020-06-02 21:47:13 -0300

    fixing margin

--- src/components/Page/components/Body/components/ProductList/components/ProductCard/index.js
+++ src/components/Page/components/Body/components/ProductList/components/ProductCard/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 const ProductCart = props => {
   return (
-    <section className='card w-20 mb-2'>
+    <section className='card w-20 mb-2 mr-2'>
       <img className='card-img-top item-img' alt='' src={props.img} />
       <div class='card-header h-50 text-center'>{props.title}</div>
       <div className='card-body'>
@@ -12,7 +12,7 @@ const ProductCart = props => {
       <div className='card-footer text-center'>
         <button
           onClick={props.addElementFunction}
-          className='btn btn-primary'
+          className='btn btn-primary btn-custom'
           id={props.id}
         >
           Adicionar ao carrinho
